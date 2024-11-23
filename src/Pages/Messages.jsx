41c94414143d18019c7cbe6cacf338a35b6d536f
gusenga thierry <127823871+draken250@users.noreply.github.com>
2024-11-23 08:51:08 +0200

    do not import

---
+++ src/Pages/Messages.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Leftbar from "../Components/Leftbar";
+import Topbar from "../Components/Topbar";
+import Rightbar from "../Components/Rightbar";
+
+function Messages() {
+  return (
+    <div className="flex flex-col items-start justify-start bg-main-body">
+      <Topbar />
+      <div className="w-full flex items-start justify-between">
+        <Leftbar />
+        <div className="flex-1 h-full min-h-svh mt-[-80px] pt-[100px] flex items-center justify-center">
+          <h1 className="text-4xl font-bold">Messages Page</h1>
+        </div>
+        <Rightbar />
+      </div>
+    </div>
+  );
+}
+
+export default Messages;
