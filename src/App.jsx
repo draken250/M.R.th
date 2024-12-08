1bce934aee6d4860261aa94bccfa1c4b1859a543
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-08 12:29:00 +0200

    Stalker-commit: src/App.jsx src/Pages/Calendar.jsx src/Pages/CalendarPage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Overview from "./Pages/Overview";
 import Patient_chart from "./Pages/Patient_chart";
-import Calendar from "./Pages/Calendar";
+import CalendarPage from "./Pages/CalendarPage";
 import Sport from "./Pages/Sport";
 import Diet from "./Pages/Diet";
 
@@ -12,7 +12,7 @@ function App() {
       <Routes>
       <Route path="/" element={<Overview />}/> 
       <Route path="/Patient_chart" element={<Patient_chart />}/> 
-      <Route path="/Calendar" element={<Calendar />}/> 
+      <Route path="/Calendar" element={<CalendarPage />}/> 
       <Route path="/Sport" element={<Sport />}/> 
       <Route path="/Diet" element={<Diet />}/> 
       </Routes>
