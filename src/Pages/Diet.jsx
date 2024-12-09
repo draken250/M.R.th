80cff1121fbcb6d6905cdb8648dc0b7b3d6fd0f0
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-09 11:13:13 +0200

    Stalker-commit: src/Pages/Diet.jsx

--- src/Pages/Diet.jsx
+++ src/Pages/Diet.jsx
@@ -121,7 +121,7 @@ function Diet() {
               </a>
             </div>
 
-            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 w-full">
+            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-5 gap-4 w-full">
               <div class="bg-gray-100 rounded-lg overflow-hidden">
                 <div class="relative">
                   <img
