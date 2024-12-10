e33f5ec474ed4d54aa18c47d522469e19d59e937
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-10 10:26:45 +0200

    Stalker-commit: src/Pages/Overview.jsx

--- src/Pages/Overview.jsx
+++ src/Pages/Overview.jsx
@@ -21,7 +21,8 @@ function Overview() {
               <div class="heartrate w-2/3 px-4 pt-4 pb-2 bg-white text-black m-2 rounded-xl ">
                 <div className="w-full flex flex-col">
                   <div className="flex justify-between">
-                    <h1 className="text-sm font-bold">Heart Reate</h1>
+                    <h1 className="text-sm font-bold">Heart rate</h1>
+                    <div className="flex"></div>
                   </div>
                 </div>
                 <Statistics />
