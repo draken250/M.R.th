d81525ccb8dd62d553cbe8b2710ea2924e7c316f
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-10 10:31:15 +0200

    Stalker-commit: src/Pages/Overview.jsx

--- src/Pages/Overview.jsx
+++ src/Pages/Overview.jsx
@@ -23,8 +23,8 @@ function Overview() {
             <div class="stats flex  ">
               <div class="heartrate w-2/3 px-4 pt-4 pb-2 bg-white text-black m-2 rounded-xl mb-4 ">
                 <div className="w-full flex flex-col">
-                  <div className="flex justify-between">
-                    <h1 className="text-sm">Heart rate</h1>
+                  <div className="flex justify-between items-center">
+                    <h1 className="text-base">Heart rate</h1>
                     <div className="flex items-center gap-2">
                       <button class="flex items-center justify-center gap-2 px-4 py-2.5 text-sm rounded-full ring-1 ring-stone-200 hover:bg-stone-200">
                         <LuUpload className="text-lg" />
@@ -36,7 +36,7 @@ function Overview() {
                       </button>
                     </div>
                   </div>
-                  <h1 className="text-2xl flex items-center gap-1 mb-3">
+                  <h1 className="text-3xl flex items-center gap-2 mb-3">
                     88 <span className="opacity-40">BPM</span> <GoHeartFill className="text-main-color" />
                   </h1>
                 </div>
