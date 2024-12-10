be982303e27872358142b40154a54a0d5bb243c5
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-10 10:28:45 +0200

    Stalker-commit: src/Pages/Overview.jsx

--- src/Pages/Overview.jsx
+++ src/Pages/Overview.jsx
@@ -4,6 +4,7 @@ import Topbar from "../Components/Topbar";
 import Rightbar from "../Components/Rightbar";
 import Statistics from "../Components/Statistics";
 import { RxMixerHorizontal } from "react-icons/rx";
+import { LuChevronDown } from "react-icons/lu";
 
 function Overview() {
   return (
@@ -28,9 +29,9 @@ function Overview() {
                         <RxMixerHorizontal className="text-lg" />
                         <span>Export</span>
                       </button>
-                      <button class="flex items-center justify-center gap-2 px-4 py-2.5 text-sm rounded-full ring-1 ring-stone-200 hover:bg-stone-200">
-                        <RxMixerHorizontal className="text-lg" />
+                      <button class="flex items-center justify-center gap-2 pl-4 pr-3 py-2.5 text-sm rounded-full ring-1 ring-stone-200 hover:bg-stone-200">
                         <span>Weekly</span>
+                        <LuChevronDown className="text-lg" />
                       </button>
                     </div>
                   </div>
